bf94a64b7354c77443d4945061c049375e8c7296
HamadaDere <hamadad54321@gmail.com>
2024-11-13 08:38:09 -0800

    custom row contain vertical divider widget

--- lib/widgets/row_widget/custom_row_contain_circle_avatar_widget.dart
+++ lib/widgets/row_widget/custom_row_contain_circle_avatar_widget.dart
@@ -1,3 +1,4 @@
+import 'package:business_card_app/widgets/row_widget/custom_row_contain_vertical_divider_widget.dart';
 import 'package:flutter/material.dart';
 
 class CustomRowContainCircleAvatar extends StatelessWidget {
@@ -6,7 +7,13 @@ class CustomRowContainCircleAvatar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
-      children: [],
+      children: [
+        Stack(
+          children: [
+            CustomRowContainVerticalDividerWidget(),
+          ],
+        ),
+      ],
     );
   }
 }
---
+++ lib/widgets/row_widget/custom_row_contain_vertical_divider_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomRowContainVerticalDividerWidget extends StatelessWidget {
+  const CustomRowContainVerticalDividerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        SizedBox(
+          width: 10,
+        ),
+        VerticalDivider(
+          thickness: 5,
+          width: 17,
+          color: Color(
+            0xffE2BB59,
+          ),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        VerticalDivider(
+          thickness: 5,
+          width: 17,
+          color: Color(
+            0xffE2BB59,
+          ),
+        ),
+      ],
+    );
+  }
+}
